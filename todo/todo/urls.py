f65623cbbffed4e3fd3d5f5af64a0ba686815602
RinoRino1 <ahlidinovna@gmail.com>
2021-01-22 18:21:36 +0600

    new tests

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path("", homepage, name="home"),
     path("test/", test, name="test"),
     path("test2/", second)
-    path("test3/", third)
+    # path("test3/", third)
 ]   +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
