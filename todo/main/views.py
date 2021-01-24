a45015c54c3f969d0d9f742c043655a442226537
RinoRino1 <ahlidinovna@gmail.com>
2021-01-24 17:22:12 +0600

    todo list in test page

--- todo/main/views.py
+++ todo/main/views.py
@@ -6,7 +6,7 @@ def homepage(request):
 
 def test(request):
     todo_list = ToDo.objects.all()
-    return render(request, 'test.html')
+    return render(request, 'test.html', {"todo_list": todo_list})
 
 def second(request):
     return HttpResponse("test 2 page")
