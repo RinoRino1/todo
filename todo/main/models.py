509ede2946e9fb2c4709f3051ad4fe614f0cac78
RinoRino1 <ahlidinovna@gmail.com>
2021-01-24 20:20:33 +0600

    mark todo

--- todo/main/models.py
+++ todo/main/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class ToDo(models.Model):
     text = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
